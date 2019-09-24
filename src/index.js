f33094681b831ac816d261d99c9d76e9ac775b4a
RomanGorenkov <roman.gorenkov.one@gmail.com>
2019-09-24 17:42:50 +0300

    72

--- src/index.js
+++ src/index.js
@@ -5,6 +5,20 @@ function eval() {
 
 function expressionCalculator(expr) {
     // write your solution here
+    let splitArray = expr.split('');
+    let rightBrackets = splitArray.filter(rightBrackets => rightBrackets === ')').length;
+    let leftBrackets = splitArray.filter(leftBrackets => leftBrackets === '(').length;
+
+    if (rightBrackets !== leftBrackets) {
+        throw new Error('ExpressionError: Brackets must be paired')
+    } else {
+        let result = (new Function('return ' + expr))();
+
+        if(result===Infinity){
+            throw new Error('TypeError: Devision by zero.')
+        }
+        return result;
+    }
 }
 
 module.exports = {
